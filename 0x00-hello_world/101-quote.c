6973db704ccd9e1a2e334c527d5bc05b2b0835f5
FearGod Okeke <okekefeargod@gmail.com>
2023-06-17 11:03:11 -0700

    printing without printf or puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
  * write - to print the string.
  *
  * Return: 1 to mean successful execution.
- */ 
+ */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
