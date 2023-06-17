820c297491542749c494428a3651101d02e707b1
FearGod Okeke <okekefeargod@gmail.com>
2023-06-17 07:51:52 -0700

    update README for the print string task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * puts - Prints ""Programming is like building a multilingual puzzle"
+ */
 int main (void)
 {
-	const char *memo = "\"Programming is like building a multilingual puzzle";
+	const char *memo= "\"Programming is like building a multilingual puzzle";
+	
 	puts(memo);
-	return 0;
+	return (0);
 }
