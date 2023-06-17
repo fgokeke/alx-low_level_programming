9e173fcde136423575dac86cf793d424dd18a86b
FearGod Okeke <okekefeargod@gmail.com>
2023-06-17 09:25:56 -0700

    show the size of different variable types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - the main function.
+ *
+ * printf - shows the size of "char"
+ *
+ * printf - shows the size of "int"
+ *
+ * printf - shows the size of "long int"
+ *
+ * printf - shows the size of "long long int"
+ *
+ * printf - shows the size of "float"
+ *
+ * Return: 0 if program is successful
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+	return (0);
+}
