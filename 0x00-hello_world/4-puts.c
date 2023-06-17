013a94aa72ff6fca1ff07982e2539d1e1c011de7
FearGod Okeke <okekefeargod@gmail.com>
2023-06-17 08:11:36 -0700

    print a string with put function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
 /**
+ * This is the main funtion
+ *
  * puts - Prints ""Programming is like building a multilingual puzzle"
  *
  * Return: 0 if successful
  */
 int main(void)
 {
-	const char *memo = "\"Programming is like building a multilingual puzzle";
-	puts(memo);
-	return (0);
+        const char *memo = "\"Programming is like building a multilingual puzzle";
+        puts(memo);
+
+        return (0);
 }
