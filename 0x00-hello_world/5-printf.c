ad97d9c24f7cb6437869afa1a9c5b93da7251351
FearGod Okeke <okekefeargod@gmail.com>
2023-06-17 08:40:48 -0700

    print string using printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - the main function.
+ *
+ * printf - to print the string.
+ *
+ * Return: 0 if successful.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
