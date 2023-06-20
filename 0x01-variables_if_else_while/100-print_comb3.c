2a16990da296921a40271f7e56b486166e1f39b6
FearGod Okeke <okekefeargod@gmail.com>
2023-06-20 13:16:02 -0700

    print combination of 2 digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - to print the smallest combination 2 digits
+ *
+ * Return: 0 for successful execution
+ */
+int main(void)
+{
+	int i;
+	int a;
+	int comma = 44;
+	int space = 32;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (a = 48; a < 58; a++)
+		
+		{
+			if (i != a && i < a)
+			{
+				putchar(i);
+				putchar(a);
+				if (i != 57 || a != 57)
+				{
+					putchar(comma);
+					putchar(space);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	
+	return (0);	
+}
