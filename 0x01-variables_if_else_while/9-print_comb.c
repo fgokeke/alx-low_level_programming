f2c9372ece00eeed68dc514e64f0e7e74825ceed
FearGod Okeke <okekefeargod@gmail.com>
2023-06-20 11:02:34 -0700

    print 0 to 9 with commas and spaces

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,16 @@ int main(void)
 	int comma = 44;
 	int space = 32;
 
-	for (i = 48; i <= 57; i++)
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
+		if (i != 57)
+		{
 		putchar(comma);
 		putchar(space);
+		}
 	}
+	putchar('\n');
 
 	return (0);
 }
