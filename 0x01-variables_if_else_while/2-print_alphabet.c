0414c75bd349895ec02c602d99b8cbef0624d2a4
FearGod Okeke <okekefeargod@gmail.com>
2023-06-19 18:49:45 -0700

    print the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * char - variable type for the alphabets
+ *
+ * Return: 0 at successful execution
+ */
+int main(void)
+{
+	char First_Alph = 'a';
+	char Last_Alph = 'z';
+
+	while (First_Alph <= Last_Alph)
+	{
+		putchar(First_Alph);
+		First_Alph++;
+	}
+
+	return (0);
+}
