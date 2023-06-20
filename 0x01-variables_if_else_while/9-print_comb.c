049501589092d286c7d4fa8ad992e540a870ef74
FearGod Okeke <okekefeargod@gmail.com>
2023-06-20 09:50:10 -0700

    print 0 to 9 with space and comma

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - to print 0 to 9
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int i;
+	int comma = 44;
+	int space = 32;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		putchar(comma);
+		putchar(space);
+	}
+	putchar('\n');
+
+	return (0);
+}
