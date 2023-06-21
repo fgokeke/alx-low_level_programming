ef1a95e1b74c31c9450273a84cc33bca0b4d7f29
FearGod Okeke <okekefeargod@gmail.com>
2023-06-21 14:58:58 -0700

    function to print the alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * void - to print the alphabets
+ *
+ * Return: does not return a value
+ */
+void print_alphabet(void)
+{
+	char first_alph = 'a';
+	char last_alph = 'z';
+
+	while (first_alph <= last_alph)
+	{
+		_putchar(first_alph);
+		first_alph++;
+	}
+	_putchar('\n');
+
+	return;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define PUTCHAR_H
 
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
