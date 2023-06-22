afb5f3f022be34b45b67b3e8d452356dd6446291
FearGod Okeke <okekefeargod@gmail.com>
2023-06-22 16:22:57 -0700

    print times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times-table - to print the times table up to 9
+ *
+ * Return: there is no return value
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+	int product;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			product = (a * b);
+			if (product < 10)
+			{
+				_putchar(product + '0');
+			}
+			else
+				{
+					_putchar(product / 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+					_putchar(',');
+					_putchar(' ');
+		}
+					_putchar('\n');
+	}
+}
