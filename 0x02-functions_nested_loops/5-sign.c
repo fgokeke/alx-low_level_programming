3ed088959d79695bdded5d3aad02ed7d56b1d035
FearGod Okeke <okekefeargod@gmail.com>
2023-06-22 05:44:45 -0700

    check the signs of integers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - to check for the sign of numbers
+ * @n: the integers that are to be checked
+ *
+ * Return: 0 on success
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return(1);
+	}
+		else if (n == 0)
+		{
+			_putchar('0');
+			return(0);
+		}
+		else if (n < 0)
+		{
+			_putchar('-');
+			return(-1);
+		}
+
+	return(0);
+}
