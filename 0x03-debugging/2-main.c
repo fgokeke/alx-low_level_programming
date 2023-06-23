af6e30df95bb6d00d283165f789f756ff6cf8b00
FearGod Okeke <okekefeargod@gmail.com>
2023-06-23 04:36:53 -0700

    correct code to print the largest number

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -5,7 +5,6 @@
 * main - prints the largest of 3 integers
 * Return: 0
 */
-
 int main(void)
 {
 	int a, b, c;
@@ -14,7 +13,6 @@ int main(void)
 	a = 972;
 	b = -98;
 	c = 0;
-
 	largest = largest_number(a, b, c);
 
 	printf("%d is the largest number\n", largest);
