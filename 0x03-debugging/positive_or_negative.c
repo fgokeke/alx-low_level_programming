6c5f72982f24b81af59215bcf96b01b1a75149a6
FearGod Okeke <okekefeargod@gmail.com>
2023-06-23 02:41:01 -0700

    debugging my first file

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -9,17 +9,17 @@
  * Return: no return value
  */
 void positive_or_negative(int i)
-{       	
-        if (i > 0)
-        {
-                printf("%d is positive\n", i);
-        }
-        else if (i == 0)
-        {
-                printf("%d is zero\n", i);
-        }
-        else if (i < 0)
-        {
-                printf("%d is negative\n", i);
-        }
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i < 0)
+	{
+	printf("%d is negative\n", i);
+	}
 }
