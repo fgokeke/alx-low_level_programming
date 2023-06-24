efbdeac6effad6c6a76c4c97308b72d5a0116085
FearGod Okeke <okekefeargod@gmail.com>
2023-06-24 05:13:46 -0700

    print the 9x9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,29 +7,32 @@
  */
 void times_table(void)
 {
-	int a;
-	int b;
-	int product;
+	int a, b, product, c, d;
 
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
 		{
-			product = (a * b);
-			if (product < 10)
+			product = a * b;
+			if (product > 9)
 			{
+				c = product % 10;
+				d = (product - c) / 10;
 				_putchar(',');
 				_putchar(' ');
-				_putchar(' ');
-				_putchar(product + '0');
+				_putchar(d + '0');
+				_putchar(c + '0');
 			}
 			else
+			{
+				if (b != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar(product / 10 + '0');
-					_putchar(product % 10 + '0');
+					_putchar(' ');
 				}
+				_putchar(product + '0');
+			}
 		}
 		_putchar('\n');
 	}
