d6f7d6b19d2058535a295349299f326fc29157c5
FearGod Okeke <okekefeargod@gmail.com>
2023-06-25 12:53:12 -0700

    print first 98 fibonacci numbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -16,12 +16,12 @@ int main(void)
 	printf("%lu, ", n1);
 	printf("%lu, ", n2);
 
-	for (i = 3; i < 99; i++)
+	for (i = 3; i < 92; i++)
 	{
 		next = n1 + n2;
 		n1 = n2;
 		n2 = next;
-		if (i != 98)
+		if (i != 92)
 		{
 			printf("%lu, ", next);
 		}
