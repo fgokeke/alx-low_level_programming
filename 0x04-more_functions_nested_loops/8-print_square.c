792356923e94560832e6c203e93bb012878751ef
FearGod Okeke <okekefeargod@gmail.com>
2023-06-26 01:58:35 -0700

    print squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - to print a square with #
+ * @size: the size of the square
+ * Return: no return value
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
+}
