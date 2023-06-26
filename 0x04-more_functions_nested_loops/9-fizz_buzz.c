72392388c8ba82581b6b33d97d0c8e20002ce27d
FearGod Okeke <okekefeargod@gmail.com>
2023-06-26 03:09:22 -0700

    fizzbuzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,26 +15,23 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			if (i != 100)
-			{
-				printf("%d ", i);
-			}
-			else
-			{
 			printf("%d", i);
-			}
+		}
+		if (i != 100)
+		{
+			printf(" ");
 		}
 	}
 	printf("\n");
