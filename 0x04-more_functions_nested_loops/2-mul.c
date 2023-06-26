bf6223cdeb85fc15e7ebb6d9352d7d0d702d0de2
FearGod Okeke <okekefeargod@gmail.com>
2023-06-25 23:17:50 -0700

    multiply 2 integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,14 +2,15 @@
 
 /**
  * mul - two multiply two integers
- * @a - first integer
- * @b - second integer
+ * @a: first integer
+ * @b: second integer
  *
  * Return: the result of multiplication on successful execution
  */
 int mul(int a, int b)
 {
 	int mul;
+
 	mul = a * b;
 
 	return (mul);
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 1. Write a function that checks for uppercase character.
 2. Write a function that checks for a digit (0 through 9).
 3. Write a function that multiplies two integers.
+4. Write a function that prints the numbers, from 0 to 9, followed by a new line.
