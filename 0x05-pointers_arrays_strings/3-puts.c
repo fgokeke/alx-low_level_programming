e1f069f2a62af87a1e95df5cf552e0914ccd9fc8
FearGod Okeke <okekefeargod@gmail.com>
2023-06-28 13:07:25 -0700

    print the string to the stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _puts - to print a string, followed by a new line, to stdout.
+ * @str: the string to be printed
+ *
+ * Return: no return value
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
