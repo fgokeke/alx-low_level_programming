00f918a4ebc0926d57b222c82b661c6a50e37537
FearGod Okeke <okekefeargod@gmail.com>
2023-07-02 18:45:57 -0700

    capitalise all the words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * cap_string: capitalizes all words of a string.
+ * @s: the string to be considered
+ *
+ * Return: returns the string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		while (!(s[i] <= 'z' && s[i] >= 'a'))
+		i++;
+		if (s[i - 1] == ',' ||
+		s[i - 1] == ';' ||
+		s[i - 1] == '.' ||
+		s[i - 1] == ' ' ||
+		s[i - 1] == '!' ||
+		s[i - 1] == '"' ||
+		s[i - 1] == '(' ||
+		s[i - 1] == ')' ||
+		s[i - 1] == '{' ||
+		s[i - 1] == '}' ||
+		s[i - 1] == '\n' ||
+		s[i - 1] == '\t' ||
+		s[i - 1] == '?' ||
+		s[i - 1] == 0)
+		s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -82,4 +82,6 @@ int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 
 char *string_toupper(char *s);
+
+char *cap_string(char *s);
 #endif
