194e8d7baa272a38c6dec8b1d277c7d159cb7b24
FearGod Okeke <okekefeargod@gmail.com>
2023-07-05 11:08:21 -0700

    prints the chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * print_chessboard
+ * print_chessboard - a function that prints the chessboard.
  * @a: the number of rows in the 2d array.
  *
  * Return: no return value
@@ -10,7 +10,7 @@
 void print_chessboard(char (*a)[8])
 {
 	int x, b;
-	
+
 	for (x = 0; x < 8; x++)
 	{
 		for (b = 0; b < 8; b++)
