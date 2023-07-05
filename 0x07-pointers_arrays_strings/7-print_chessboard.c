489c27d391128ea024771a14033ebeef4a3fb968
FearGod Okeke <okekefeargod@gmail.com>
2023-07-05 11:06:32 -0700

    prints the chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_chessboard
+ * @a: the number of rows in the 2d array.
+ *
+ * Return: no return value
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, b;
+	
+	for (x = 0; x < 8; x++)
+	{
+		for (b = 0; b < 8; b++)
+		{
+			_putchar(a[x][b]);
+		}
+		_putchar('\n');
+	}
+}
