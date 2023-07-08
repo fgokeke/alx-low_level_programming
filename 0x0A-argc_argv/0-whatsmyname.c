00d8a2caa6cc900387760845f8dedcd41e26ae16
FearGod Okeke <okekefeargod@gmail.com>
2023-07-08 14:24:34 -0700

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,12 +4,14 @@
 /**
  * main - program that prints its name, followed by a new line.
  * @argc: to count the number of command line arguments.
- * @argv: the arrays pointing to the arguments
+ * @argv: the arrays pointing to the arguments.
  *
  * Return: Always 0.
  */
-int __attribute__ ((unused)) main(int argc __attribute__ ((unused)), char *argv[] __attribute__ ((unused)))
+int main(int argc, char *argv[])
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
 
 	return (0);
