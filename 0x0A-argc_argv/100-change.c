cca9e626159334e355cdd6591d9d4e874d4bd03d
FearGod Okeke <okekefeargod@gmail.com>
2023-07-09 12:50:09 -0700

    no of coins to change cents

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int change_cent(int a);
 /**
  * main - prints the minimum number of coins
  * to make change for an amount of money.
@@ -12,57 +11,44 @@ int change_cent(int a);
  */
 int main(int argc, char **argv)
 {
-	int a;
+	int cents;
+	int coins_no = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	a = atoi(argv[1]);
-	if (a < 0)
+	cents = atoi(argv[1]);
+	if (cents < 0)
 	{
 		printf("%d\n", 0);
 	}
-	return (change_cent(a));
-}
+	while (cents > 0)
+	{
+		coins_no++;
+		if ((cents - 25) >= 0)
+		{
+			cents -= 25;
+			continue;
+		}
+		if ((cents - 10) >= 0)
+		{
+			cents -= 10;
+			continue;
+		}
+		if ((cents - 5) >= 0)
+		{
+			cents -= 5;
+		}
+		if ((cents - 2) >= 0)
+		{
+			cents -= 2;
+			continue;
+		}
+		cents--;
+	}
+	printf("%d\n", coins_no);
 
-/**
- * change_cent - to calculate min. coins to make change.
- * @a: the money to be changed
- *
- * Return: Always 0.
- */
-int change_cent(int a)
-{
-	int b, c, i, j;
-
-		if (a < 10)
-		{
-			printf("%d\n", (a / 2) + (a % 2));
-		}
-		else if (a >= 10 && a < 100)
-		{
-			b = ((a / 10) + (((a % 10) / 2) + ((a % 10) % 2)));
-			printf("%d\n", b);
-		}
-		else if (a >= 100 && a < 1000)
-		{
-			i = (a / 25);
-			j = (a % 25);
-			if (j == 0)
-			{
-				printf("%d\n", i);
-			}
-			else if (j < 10)
-			{
-				printf("%d\n", i + ((j / 2) + (j % 2)));
-			}
-			else if (j >= 10 && j < 100)
-			{
-				c = ((j / 10) + (((j % 10) / 2) + ((j % 2) % 2)));
-				printf("%d\n", i + c);
-			}
-		}
 	return (0);
 }
