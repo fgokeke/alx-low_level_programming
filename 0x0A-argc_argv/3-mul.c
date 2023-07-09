d98a93a291eee6e65c70f3560528cfbf77be6284
FearGod Okeke <okekefeargod@gmail.com>
2023-07-08 17:04:22 -0700

    multiplies 2 numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,16 +14,18 @@ int main(int argc, char **argv)
 	int i;
 	int mul = 1;
 
-	if (argc < 2)
+	if (argc == 3)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+		printf("%d\n", mul);
+	}
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	for (i = 1; i < argc; i++)
-	{
-		mul *= atoi(argv[i]);
-	}
-	printf("%d\n", mul);
-
 	return (0);
 }
