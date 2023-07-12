1d738bccc107acf17e6c78eb5a1914bd1c934b4e
FearGod Okeke <okekefeargod@gmail.com>
2023-07-12 04:06:52 -0700

    an array of char

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,30 +3,30 @@
 #include <stdlib.h>
 
 /**
- * create_array - a function that creates an array of chars,
- * and initializes it with a specific char.
- * @size: size of the array.
- * @c: the char to initialize it with.
+ * create_array - creates an array of chars, and
+ * initializes it with a specific char.
+ * @size: the size of the array.
+ * @c: the char to initialize the array to.
  *
- * Return: pointer to the array.
+ * Return: a pointer to the array.
  */
 char *create_array(unsigned int size, char c)
 {
-	char *b;
-	unsigned int a;
+	char *a;
+	unsigned int i;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	b = malloc(size * sizeof(char));
-	if (b == NULL)
+	a = malloc(size * sizeof(char));
+	if (a == NULL)
 	{
 		return (NULL);
 	}
-	for (a = 0; a < size; a++)
+	for (i = 0; i < size; i++)
 	{
-		b[a] = c;
+		a[i] = c;
 	}
-	return (b);
+	return (a);
 }
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -132,12 +132,4 @@ int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
 char *create_array(unsigned int size, char c);
-
-char *_strdup(char *str)i;
-
-char *str_concat(char *s1, char *s2);
-
-int **alloc_grid(int width, int height);
-
-void free_grid(int **grid, int height);
 #endif
