c453422123ae92bb66c7cbf802dda11a5a387e95
FearGod Okeke <okekefeargod@gmail.com>
2023-07-18 05:18:33 -0700

    struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - describes a dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: name of the owner.
+ */
 struct dog
 {
 	char *name;
