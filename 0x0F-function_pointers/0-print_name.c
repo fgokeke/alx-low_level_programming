b3e6fcd0a8c3611abfdc1412a9be5863e0e0a3ac
FearGod Okeke <okekefeargod@gmail.com>
2023-07-19 08:27:03 -0700

    function that prints a name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,11 +4,11 @@
 /**
  * print_name -  a function that prints a name.
  * @name: the name to be printed.
- * @f: 
+ * @f:
  *
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);	
+	f(name);
 }
