cf004601145b35a39c6f588ad835b571a2cd58d4
FearGod Okeke <okekefeargod@gmail.com>
2023-07-19 09:05:05 -0700

    executes function on elements of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -4,9 +4,11 @@
 /**
  * array_iterator - a function that executes a function
  * given as a parameter on each element of an array
- * @array:
- * @size:
- * @action:
+ * @array: the array to be considered
+ * @size: size of the array.
+ * @action: the action to be performed on the element.
+ *
+ * Return: nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
