0b7709eeea397bd14a93d0b9b55d063607e8f6b7
FearGod Okeke <okekefeargod@gmail.com>
2023-07-27 15:02:25 -0700

    print before executing main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,6 @@ void my_constructor_function(void) __attribute__((constructor));
  */
 void my_constructor_function(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-	I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
