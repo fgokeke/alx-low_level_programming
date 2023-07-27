7f1fda8fbb2c14571fc288b970938ddc8ce1f2f0
FearGod Okeke <okekefeargod@gmail.com>
2023-07-27 15:17:00 -0700

    assembly program to print

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,25 +1,10 @@
-section .data
-    hello db 'Hello, Holberton', 0x0A  ; Null-terminated string to print
-    format db '%s', 0x0               ; Format string for printf with a newline, null-terminated
-
-section .text
-    global main
-    extern printf
-
+global   main
+	  extern    printf
 main:
-    ; Set up the stack frame
-    push rbp
-    mov rbp, rsp
-
-    ; Pass the address of the format string to printf
-    lea rdi, [format]
-    lea rsi, [hello]
-
-    ; Call printf with the format and argument
-    call printf
-
-    ; Clean up the stack frame and return from main
-    pop rbp
-    xor eax, eax
-    ret
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov   eax, 0
+	  ret
+format: db `Hello, Holberton\n`,0
 
