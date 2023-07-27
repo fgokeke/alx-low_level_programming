a0bc3c437d9fa467cf606a7594d5bad38c6a1f90
FearGod Okeke <okekefeargod@gmail.com>
2023-07-27 14:59:14 -0700

    print before executing main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void my_constructor_function(void) __attribute__((constructor));
+
+/**
+ * my_constructor_function - to print
+ * before main function.
+ *
+ * Return: Nothing.
+ */
+void my_constructor_function(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+	I bore my house upon my back!\n");
+}
