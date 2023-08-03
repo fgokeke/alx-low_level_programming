653397aa5ff2f6ff6be55e53ebd98a24f69fef44
FearGod Okeke <okekefeargod@gmail.com>
2023-08-03 13:00:35 -0700

    converts binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <string.h>
+#include <stdlib.h>
 
 /**
  * binary_to_uint -  a function that converts a binary
@@ -12,11 +12,13 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int weight = 1, index, str_length = strlen(b);
+	int weight = 1, index, str_length = 0;
 	unsigned int decimal = 0;
 
 	if (b == NULL)
 		return (0);
+	while (b[str_length])
+		str_length++;
 	for (index = str_length - 1; index >= 0; index--)
 	{
 		if (b[index] < '0' || b[index] > '1')
