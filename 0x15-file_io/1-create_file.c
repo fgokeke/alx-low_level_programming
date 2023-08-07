6f0827a28dc81fbe932e84674c268a0e1b4bd7af
FearGod Okeke <okekefeargod@gmail.com>
2023-08-07 12:06:43 -0700

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,22 +16,19 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-	if (fd == -1)
-		return (-1);
-
-	while (text_content[str_length])
+	if (text_content != NULL)
 	{
-		str_length++;
-	}
-	if (str_length > 0)
-	{
-		bytes_written = write(fd, text_content, str_length);
-		if (bytes_written == -1)
+		while (text_content[str_length])
 		{
-			close(fd);
-			return (-1);
+			str_length++;
 		}
+	}
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	bytes_written = write(fd, text_content, str_length);
+	if (bytes_written == -1 || fd == -1)
+	{
+		close(fd);
+		return (-1);
 	}
 	close(fd);
 
